89a71df79e924ae79e6973533b1f955ad4bb7042
DHRUV <sutariyadhruv20@gmail.com>
2026-09-14 21:21:45 +0530

    chore: remove legacy Node.js backend files in preparation for FastAPI implementation

--- src/backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardSummary, getSystemStatus } = require('../controllers/dashboardController');
-
-router.get('/summary', getDashboardSummary);
-router.get('/system-status', getSystemStatus);
-
-module.exports = router;
--- src/backend/src/routes/mitreRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTechniques, getTechniqueById } = require('../controllers/mitreController');
-
-router.get('/', getTechniques);
-router.get('/:id', getTechniqueById);
-
-module.exports = router;
